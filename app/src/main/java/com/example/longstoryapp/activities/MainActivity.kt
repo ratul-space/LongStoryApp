fd349f27b07708884679760ecf1046773640ec4e
ratul-space <to.ratul.hosen@gmail.com>
2025-04-14 21:54:02 +0600

    update

--- app/src/main/java/com/example/longstoryapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/longstoryapp/activities/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.example.longstoryapp
+package com.example.longstoryapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.longstoryapp.R
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.long_story_app)
     }
 }
